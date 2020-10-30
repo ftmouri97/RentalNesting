eb0f70022b24e8136215e895d394cce470e47122
Sany-alam <playerc950@gmail.com>
2020-10-30 21:51:39 +0600

    feature image problem solved

--- database/migrations/2020_10_30_064106_create_feature_images_table.php
+++ database/migrations/2020_10_30_064106_create_feature_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFetaureImagesTable extends Migration
+class CreateFeatureImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -30,6 +30,6 @@ class CreateFetaureImagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('fetaure_images');
+        Schema::dropIfExists('feature_images');
     }
 }
