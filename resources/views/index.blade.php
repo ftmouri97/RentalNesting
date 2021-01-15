7f2028e0bf18da47254f319781b5d8680d9a4182
Monira-Mumu <monirajahan553@gmail.com>
2021-01-15 21:11:01 +0600

    Demo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -232,19 +232,19 @@
             <div class="col-xl-4 col-md-4">
                 <div class="single_counter">
                     <h3> <span class="counter">200</span> <span>+</span> </h3>
-                    <p>Properties for sale</p>
+                    <p>Properties for Rent</p>
                 </div>
             </div>
             <div class="col-xl-4 col-md-4">
                 <div class="single_counter">
                     <h3> <span class="counter">300</span></h3>
-                    <p>Properties for sale</p>
+                    <p>Properties for Rent</p>
                 </div>
             </div>
             <div class="col-xl-4 col-md-4">
                 <div class="single_counter">
                     <h3> <span class="counter">15</span></h3>
-                    <p>Properties for sale</p>
+                    <p>Properties for Rent</p>
                 </div>
             </div>
         </div>
