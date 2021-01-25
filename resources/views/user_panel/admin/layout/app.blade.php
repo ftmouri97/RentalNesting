650738af1ccdc04ad4a38c7fa27088f36142f13f
n0k1b <nokibevon7@gmail.com>
2021-01-25 23:34:28 +0600

    virtual payment add

--- resources/views/user_panel/admin/layout/app.blade.php
+++ resources/views/user_panel/admin/layout/app.blade.php
@@ -6,7 +6,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Melody Admin</title>
+  <title>Accomodation</title>
   <!-- plugins:css -->
   <link rel="stylesheet" href="{{ asset('assets/melody') }}/vendors/iconfonts/font-awesome/css/all.min.css">
   <link rel="stylesheet" href="{{ asset('assets/melody') }}/vendors/css/vendor.bundle.base.css">
