bce885970ba67e129ca753331b34d25d56ed13b2
Monira-Mumu <monirajahan553@gmail.com>
2021-01-26 14:43:18 +0600

    commit

--- resources/views/owner/layout/app.blade.php
+++ resources/views/owner/layout/app.blade.php
@@ -28,7 +28,7 @@
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row default-layout-navbar">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
          <a class="navbar-brand brand-logo" href="index-2.html"><img src="{{asset('assets/melody')}}/images/logo5.png" alt="logo"/></a>
-          <a class="navbar-brand brand-logo-mini" href="index-2.html"><img src="{{asset('assets/melody')}}/images/logo-mini.svg" alt="logo"/></a>
+          
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-stretch justify-content-end">
           <button class="navbar-toggler navbar-toggler align-self-right" type="button" data-toggle="minimize">
